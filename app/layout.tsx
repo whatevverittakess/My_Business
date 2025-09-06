97afebd6299f3735735a1146ecbbf3aa3d720cfc
whatevverittakess <whatevverittakess@gmail.com>
2025-09-06 17:05:07 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });

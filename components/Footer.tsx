cb0e65f9fd6534bf5a19f5bf005d90c2d151aea3
whatevverittakess <whatevverittakess@gmail.com>
2025-09-07 12:25:35 +0300

    Updated Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Facebook, Instagram, Twitter, Youtube, Dumbbell } from 'lucide-react';
-import { FaTiktok } from 'react-icons/fa';
 
 interface FooterProps {
   translations: any;
@@ -29,8 +28,8 @@ export function Footer({ translations }: FooterProps) {
               <a href="https://www.instagram.com/whatteverittakes?igsh=dTE5d3UxNXBsZHJy" className="text-gray-400 hover:text-[#D4AF37] transition-colors">
                 <Instagram className="h-6 w-6" />
               </a>
-              <a href="https://www.tiktok.com/@what.ever.it.takes?is_from_webapp=1&sender_device=pc" className="text-gray-400 hover:text-[#D4AF37] transition-colors">
-                <FaTiktok className="h-6 w-6" />
+
+              
               </a>
               <a href="https://www.youtube.com/@whatteverittakes" className="text-gray-400 hover:text-[#D4AF37] transition-colors">
                 <Youtube className="h-6 w-6" />
